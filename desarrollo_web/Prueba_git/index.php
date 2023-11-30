ba8f18bd9b0aca2baadb397c001645178de923fa
AlessCaballero <150315738+AlessCaballero@users.noreply.github.com>
2023-11-30 10:58:39 -0500

    Update index.php

--- desarrollo_web/Prueba_git/index.php
+++ desarrollo_web/Prueba_git/index.php
@@ -3,9 +3,9 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="styles/style.css">
+	<link rel="stylesheet" href="estilos.css">
 	<link rel="icon" href="https://freesvg.org/img/1534129544.png">
-	<title>Document</title>
+	<title>ADSO</title>
 </head>
 <body>
 	<header>
